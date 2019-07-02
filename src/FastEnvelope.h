3f53d9a2090e046f116f76f284e4190ebabbd546
wangbolun300 <wangbolun@buaa.edu.cn>
2019-07-03 04:40:21 +0800

    add sth

--- src/FastEnvelope.h
+++ src/FastEnvelope.h
@@ -156,7 +156,124 @@ namespace fastEnvelope {
 		}
 
 		
-		
+		template<typename T>
+		static int orient3D_LPI_filtered_multiprecision(
+			T px, T py, T pz, T qx, T qy, T qz,
+			T rx, T ry, T rz, T sx, T sy, T sz, T tx, T ty, T tz,
+			T ax, T ay, T az, T bx, T by, T bz, T cx, T cy, T cz, const std::function<int(T)> &checker) {
+			T a11, a12, a13, a21, a22, a23, a31, a32, a33, d21, d31, d22, d32, d23, d33;
+			T px_rx, py_ry, pz_rz, px_cx, py_cy, pz_cz;
+			T a2233, a2133, a2132;
+			T d, n;
+			T d11, d12, d13;
+			T d2233, d2332, d2133, d2331, d2132, d2231;
+			T det;
+
+			a11 = (px - qx);
+			a12 = (py - qy);
+			a13 = (pz - qz);
+			a21 = (sx - rx);
+			a22 = (sy - ry);
+			a23 = (sz - rz);
+			a31 = (tx - rx);
+			a32 = (ty - ry);
+			a33 = (tz - rz);
+			a2233 = ((a22 * a33) - (a23 * a32));
+			a2133 = ((a21 * a33) - (a23 * a31));
+			a2132 = ((a21 * a32) - (a22 * a31));
+			d = (((a11 * a2233) - (a12 * a2133)) + (a13 * a2132));
+			int flag1 = checker(d);
+			if (flag1 == -2) {
+				return 100;// not enough precision
+			}
+			if (flag1 == 0) {
+				return -2;// not exist
+			}
+			// The almost static filter for 'd' might be moved here
+
+			px_rx = px - rx;
+			py_ry = py - ry;
+			pz_rz = pz - rz;
+			n = ((((py_ry)* a2133) - ((px_rx)* a2233)) - ((pz_rz)* a2132));
+
+			px_cx = px - cx;
+			py_cy = py - cy;
+			pz_cz = pz - cz;
+
+			d11 = (d * px_cx) + (a11 * n);
+			d21 = (ax - cx);
+			d31 = (bx - cx);
+			d12 = (d * py_cy) + (a12 * n);
+			d22 = (ay - cy);
+			d32 = (by - cy);
+			d13 = (d * pz_cz) + (a13 * n);
+			d23 = (az - cz);
+			d33 = (bz - cz);
+
+			d2233 = d22 * d33;
+			d2332 = d23 * d32;
+			d2133 = d21 * d33;
+			d2331 = d23 * d31;
+			d2132 = d21 * d32;
+			d2231 = d22 * d31;
+
+			det = d11 * (d2233 - d2332) - d12 * (d2133 - d2331) + d13 * (d2132 - d2231);
+
+			int flag2 = checker(det);
+			if (flag2 == -2) {
+				return 100;// not enough precision
+			}
+			if (flag2 == 1) {
+				if (flag1 == 1) {
+					return 1;
+				}
+				if (flag1 == -1) {
+					return -1;
+				}
+			}
+			if (flag2 == -1) {
+				if (flag1 == 1) {
+					return -1;
+				}
+				if (flag1 == -1) {
+					return 1;
+				}
+			}
+			return 0;
+
+			//if (d.is_class() == ZERO) {
+			//	return -2;//not exist
+			//}
+
+			//if (d.is_class() == MIXED) {
+			//	return 100;//failed in precision
+			//}
+			//if (det.is_class() == MIXED) {
+			//	return 100;//failed in precision
+			//}
+
+			//if ((det.is_class() == POSITIVE)) {
+			//	if (d.is_class() == POSITIVE) {
+			//		return 1;
+			//	}
+			//	if (d.is_class() == NEGATIVE) {
+			//		return -1;
+			//	}
+			//}
+			//if ((det.is_class() == NEGATIVE)) {
+			//	if (d.is_class() == POSITIVE) {
+			//		return -1;
+			//	}
+			//	if (d.is_class() == NEGATIVE) {
+			//		return 1;
+			//	}
+			//}
+
+			//return 0;
+
+
+
+		}
 
 
 	};
