03c7130e13ff75700b883cf2cdea46260e4647bf
wangbolun300 <wangbolun@buaa.edu.cn>
2019-08-20 05:34:49 +0800

    before modifing into command line module

--- src/main.cpp
+++ src/main.cpp
@@ -612,8 +612,8 @@ std::vector<std::array<Vector3, 3>> read_CSV_triangle(const string inputFileName
 }
 
 void test_in_wild() {
-	string inputFileName = "D:\\vs\\fast_envelope_csv\\thingi10k_debug\\100729\\100729.stl_env.csv";
-	string input_surface_path1 = "D:\\vs\\fast_envelope_csv\\thingi10k_debug\\100729\\Diff_Casing_Side_2.A.9.stl";
+	string inputFileName = "D:\\vs\\fast_envelope_csv\\thingi10k_debug\\100339\\100339.stl_env.csv";
+	string input_surface_path1 = "D:\\vs\\fast_envelope_csv\\thingi10k_debug\\100339\\Stonehenge_Piece1_X1.stl";
 	vector<int> outenvelope;
 	std::vector<std::array<Vector3, 3>> triangles = read_CSV_triangle(inputFileName, outenvelope);
 
