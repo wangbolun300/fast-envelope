55c7ec64470f65bf799a18468358d9350b871eac
Teseo Schneider <teseo@bluewin.ch>
2021-09-29 11:44:26 -0700

    switched to predicates inside geogram to cleanup symbols

--- src/external/Predicates.cpp
+++ src/external/Predicates.cpp
@@ -1,24 +1,22 @@
 #include <fastenvelope/Predicates.hpp>
 
-extern "C" void exactinit();
-extern "C" fastEnvelope::Scalar orient3d(const fastEnvelope::Scalar *pa, const fastEnvelope::Scalar *pb, const fastEnvelope::Scalar *pc, const fastEnvelope::Scalar *pd);
-extern "C" fastEnvelope::Scalar orient2d(const fastEnvelope::Scalar *pa, const fastEnvelope::Scalar *pb, const fastEnvelope::Scalar *pc);
-
 #ifdef ENVELOPE_WITH_GEO_PSM
 #include <Predicates_psm.h>
 #endif
 #ifdef ENVELOPE_WITH_GEO
 #include <geogram/delaunay/delaunay_3d.h>
+#else
+#include <igl/predicates/predicates.h>
 #endif
 
-namespace fastEnvelope {
-	namespace {
+namespace fastEnvelope
+{
+	namespace
+	{
 		void init()
 		{
 #ifndef ENVELOPE_WITH_GEO
-			static bool initialized = false;
-			exactinit();
-			initialized=true;
+			igl::predicates::exactinit();
 #endif
 		}
 	}
@@ -28,7 +26,8 @@ namespace fastEnvelope {
 	const int Predicates::ORI_NEGATIVE;
 	const int Predicates::ORI_UNKNOWN;
 
-	int Predicates::orient_3d(const Vector3& p1, const Vector3& p2, const Vector3& p3, const Vector3& p4) {
+	int Predicates::orient_3d(const Vector3 &p1, const Vector3 &p2, const Vector3 &p3, const Vector3 &p4)
+	{
 		init();
 #ifdef ENVELOPE_WITH_GEO
 		const int result = -GEO::PCK::orient_3d(p1.data(), p2.data(), p3.data(), p4.data());
@@ -36,12 +35,12 @@ namespace fastEnvelope {
 		const Scalar result = orient3d(p1.data(), p2.data(), p3.data(), p4.data());
 #endif
 
-//		if (result > SCALAR_ZERO)
-//			return ORI_POSITIVE;
-//		else if (result < -SCALAR_ZERO)
-//			return ORI_NEGATIVE;
-//		else
-//			return ORI_ZERO;
+		//		if (result > SCALAR_ZERO)
+		//			return ORI_POSITIVE;
+		//		else if (result < -SCALAR_ZERO)
+		//			return ORI_NEGATIVE;
+		//		else
+		//			return ORI_ZERO;
 
 		if (result > 0)
 			return ORI_POSITIVE;
@@ -67,7 +66,8 @@ namespace fastEnvelope {
 	// 		return ORI_NEGATIVE;
 	// }
 
-	Scalar Predicates::orient_3d_volume(const Vector3& p1, const Vector3& p2, const Vector3& p3, const Vector3& p4) {
+	Scalar Predicates::orient_3d_volume(const Vector3 &p1, const Vector3 &p2, const Vector3 &p3, const Vector3 &p4)
+	{
 		init();
 #ifdef ENVELOPE_WITH_GEO
 		const int ori = -GEO::PCK::orient_3d(p1.data(), p2.data(), p3.data(), p4.data());
@@ -80,7 +80,8 @@ namespace fastEnvelope {
 			return (p1 - p4).dot((p2 - p4).cross(p3 - p4)) / 6;
 	}
 
-	int Predicates::orient_2d(const Vector2& p1, const Vector2& p2, const Vector2& p3) {
+	int Predicates::orient_2d(const Vector2 &p1, const Vector2 &p2, const Vector2 &p3)
+	{
 		init();
 #ifdef ENVELOPE_WITH_GEO
 		const int result = -GEO::PCK::orient_2d(p1.data(), p2.data(), p3.data());
